b941f22e9b359c80f1e80d3bbb8c7efddeaeea30
Sofía Sonia Martínez <zunildaramivecchi@gmail.com>
2023-04-03 19:05:01 -0300

    feat (add file REST): paremeters rest theory

---
+++ src/parameters-REST.ts
@@ -0,0 +1,14 @@
+function PrintInstruments(log : string, ...isntruments : string[]) : void
+{
+	console.log(log);
+	instruments.forEach
+	(
+		instrument=>
+		{ 
+			console.log(instrument));
+		}
+	);
+}
+PrintInstruments('Music Sho Inventory', 'Guitar', 'Drums', 'Clarinet', 'Clavinova');
+//Set arguments automated.
+
